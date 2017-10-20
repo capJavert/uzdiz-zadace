c8ffa1fa46897cfe67d8860d7fb8e24dc9c930a3
capJavert <ante.baric3@gmail.com>
2017-10-21 01:55:55 +0200

    added argument paths handling

--- src/hr/foi/uzdiz/antbaric/zadaca_1/components/CliConfigurationBuilder.java
+++ src/hr/foi/uzdiz/antbaric/zadaca_1/components/CliConfigurationBuilder.java
@@ -6,9 +6,11 @@
 package hr.foi.uzdiz.antbaric.zadaca_1.components;
 
 import hr.foi.uzdiz.antbaric.zadaca_1.models.Configuration;
-
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CliConfigurationBuilder implements ConfigurationBuilder {
+
     private final Configuration configuration;
 
     public CliConfigurationBuilder() {
@@ -23,57 +25,67 @@ public class CliConfigurationBuilder implements ConfigurationBuilder {
     @Override
     public ConfigurationBuilder setSeed(String seed) {
         this.configuration.setSeed(Integer.parseInt(seed));
-        
+
         return this;
     }
-    
+
     @Override
     public ConfigurationBuilder setPlacesFilePath(String placesFilePath) {
-        this.configuration.setPlacesFilePath(placesFilePath);
-        
+        this.configuration.setPlacesFilePath(absolutePath(placesFilePath));
+
         return this;
     }
 
     @Override
     public ConfigurationBuilder setSensorsFilePath(String sensorsFilePath) {
-        this.configuration.setSensorsFilePath(sensorsFilePath);
-        
+        this.configuration.setSensorsFilePath(absolutePath(sensorsFilePath));
+
         return this;
     }
 
     @Override
     public ConfigurationBuilder setActuatorsFielPath(String actuatorsFielPath) {
-        this.configuration.setActuatorsFielPath(actuatorsFielPath);
-        
+        this.configuration.setActuatorsFielPath(absolutePath(actuatorsFielPath));
+
         return this;
     }
 
     @Override
     public ConfigurationBuilder setAlgoritham(String algoritham) {
         this.configuration.setAlgoritham(algoritham);
-        
+
         return this;
     }
 
     @Override
     public ConfigurationBuilder setInterval(String interval) {
-        this.configuration.setInterval(Integer.parseInt(interval)*1000);
-        
+        this.configuration.setInterval(Integer.parseInt(interval) * 1000);
+
         return this;
     }
 
     @Override
     public ConfigurationBuilder setExecutionLimit(String executionLimit) {
         this.configuration.setExecutionLimit(Integer.parseInt(executionLimit));
-        
+
         return this;
     }
 
     @Override
     public ConfigurationBuilder setOutFilePath(String outFilePath) {
-        this.configuration.setOutFilePath(outFilePath);
-        
+        this.configuration.setOutFilePath(absolutePath(outFilePath));
+
         return this;
     }
-    
+
+    private static String absolutePath(String path) {
+        Path p = Paths.get(path);
+        
+        if (!p.isAbsolute()) {
+            path = System.getProperty("user.dir") + "/" + path;
+        }
+        
+        return path;
+    }
+
 }
