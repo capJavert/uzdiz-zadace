8e915db026933e6202c6f64cbb01cdc513940d89
capJavert <ante.baric3@gmail.com>
2017-10-21 02:24:12 +0200

    added random number generator

---
+++ src/hr/foi/uzdiz/antbaric/zadaca_1/components/Generator.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.foi.uzdiz.antbaric.zadaca_1.components;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ *
+ * @author javert
+ */
+public class Generator extends Random {
+
+    private static volatile Generator INSTANCE;
+
+    static {
+        INSTANCE = new Generator();
+    }
+
+    private Generator() {
+    }
+
+    public static Generator getInstance() {
+        return INSTANCE;
+    }
+
+    public Integer getStatus() {
+        return INSTANCE.nextInt(10) + 1 > 1 ? 1 : 0;
+    }
+
+    public Integer selectFrom(List<Object> list) {
+        return INSTANCE.nextInt(list.size());
+    }
+
+}
